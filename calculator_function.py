98bcd2d199359a4e232dc393b8d74c490a67450b
mhenpiyawut <piyawut.mm@gmail.com>
2025-08-31 21:42:56 +0700

    add divided function

--- calculator_function.py
+++ calculator_function.py
@@ -4,6 +4,10 @@ def multiply(a, b):
     return a * b
 def divide_by_two(a):
     return a / 2
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a / b
 a = 10
 b = 20
 c = 16
